6346a1a51de5381dac7cafb49244bfebeb560615
Arun <arungawande1995@gmail.com>
2022-04-06 20:28:01 +0530

    Added: Initialize variables

---
+++ gamblersimulator/GamblerSimulator.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.gamblersimulator;
+
+public class GamblerSimulator
+{
+    public static void main(String[] args) {
+    double INITIAL_STAKE = 100;
+    double STAKE_BET = 1;
+
+
+        System.out.println("Welcome To Gambler Simulation");
+    }
+}
