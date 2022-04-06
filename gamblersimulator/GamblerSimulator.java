68a22596e0df01648ae8f6eac1b6d0bf58031263
Arun <arungawande1995@gmail.com>
2022-04-06 20:40:29 +0530

    refactor: Win or lose

--- gamblersimulator/GamblerSimulator.java
+++ gamblersimulator/GamblerSimulator.java
@@ -2,11 +2,27 @@ package com.bridgelabz.gamblersimulator;
 
 public class GamblerSimulator
 {
+    public static final double INITIAL_STAKE = 100;
+    public static final double STAKE_BET = 1;
+    public static int stake = 0;
+
+    public static void winOrLose() {
+
+        if(Math.random()<0.5) {
+            stake++;
+            System.out.println("Win");;
+        }
+        else {
+            stake --;
+            System.out.println("Lose");
+
+        }
+    }
+
+
     public static void main(String[] args) {
-    double INITIAL_STAKE = 100;
-    double STAKE_BET = 1;
-
-
         System.out.println("Welcome To Gambler Simulation");
+        winOrLose();
+
     }
 }
